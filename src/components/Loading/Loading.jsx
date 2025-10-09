f85e7f6fa77e2eb5441995fe89cc6999c49a89b8
Abrarul Rhythm <[....email>
2025-10-09 16:14:21 +0600

    added page navigation loading animation

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -3,7 +3,7 @@ import Logo from '../../assets/logo.png';
 
 const Loading = () => {
     return (
-        <div className='flex items-center justify-center uppercase text-3xl md:text-4xl font-semibold tracking-[0.2em]'>
+        <div className='flex items-center justify-center uppercase text-3xl md:text-4xl font-bold tracking-[0.2em]'>
             <span>L</span>
             <img className='rotate-logo w-[58px] md:w-[75px] ml-4 mr-4 opacity-55' src={Logo} alt="" />
             <span>oading</span>
--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 import { ToastContainer } from 'react-toastify';
+import Loading from '../../components/Loading/Loading';
 
 const Root = () => {
+
+    const [loading, setLoading] = useState(true);
+    const [fadeOut, setFadeOut] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setFadeOut(true);
+            setTimeout(() => setLoading(false), 500);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    useEffect(() => {
+        setLoading(true);
+        setFadeOut(false);
+        const timer = setTimeout(() => {
+            setFadeOut(true);
+            setTimeout(() => setLoading(false), 500);
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    }, [location.pathname]);
+
     return (
         <>
-            <Header></Header>
-            <main>
-                <Outlet></Outlet>
-            </main>
-            <Footer></Footer>
+            {
+                loading ? (
+                    <div className={`h-screen flex justify-center items-center ${fadeOut ? 'scale-200 duration-700 opacity-0 invisible' : ''}`}>
+                        <Loading fadeOut={fadeOut}></Loading>
+                    </div>
+                ) : (
+                    <>
+                        <Header></Header>
+                        <main>
+                            <Outlet></Outlet>
+                        </main>
+                        <Footer></Footer>
 
-            <ToastContainer />
+                        <ToastContainer />
+                    </>
+                )
+            }
         </>
     );
 };
